629f01ecbd9db720f0ab7da0f89dfe889828a1d2
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-12 18:04:49 -0300

    Create script of deletion

---
+++ g-d-branch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_BRANCH=$(git branch | grep "^*" | tr -d "* ")
+
+BRANCH=$(git branch | fzf -m \
+    --height 100% \
+    --layout reverse \
+    --border \
+    --preview \
+        'git -c color.ui=always log --oneline $(echo {} | tr -d "*")' \
+    --print-query \
+    --color bg:#222222,preview-bg:#333333)
+
+if [ $? -eq 130 ]; then
+    echo "No branch selected or operation cancelled."
+fi
+
+if [ "$BRANCH" == "* $CURRENT_BRANCH" ]; then
+    echo "You can not delete the current branch."
+    exit 1
+fi
+
+git branch -D $BRANCH
