514b436569501069dc8cfdf9f3f18ea3504403f7
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-12 15:23:47 -0300

    Cleaning code

--- glb.sh
+++ glb.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# git switch $( git branch | fzf --height 40% --layout reverse --border --inline-info)
-# git switch $( git branch | fzf)
-
 git switch $( git branch | fzf +m --height 40% --layout reverse --border  --preview 'git diff $(echo {} | tr -d " *")')
