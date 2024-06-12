5adcb7df33196b0996198dd876bf41f7f2615509
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-12 16:09:24 -0300

    Create file with merge logic

---
+++ g-merge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CURRENT_BRANCH=$(git branch | grep "^*" | tr -d "* ")
+
+BRANCH=$(git branch | fzf +m \
+    --height 100% \
+    --header "Select a branch to Merge in this branch:" \
+    --layout reverse \
+    --border \
+    --preview \
+        'git -c color.ui=always diff $($CURRENT_BRANCH) $(echo {} | tr -d " ")' \
+    --color bg:#222222,preview-bg:#333333)
+
+
+if [ "$BRANCH" == "* $CURRENT_BRANCH" ]; then
+    echo "You can not merge the current branch."
+    exit 1
+fi
+
+if [ $? -eq 0 ]; then
+    git merge "$BRANCH"
+else
+    echo "No branch selected or operation cancelled."
+fi
