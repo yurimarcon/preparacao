12a3e4fde26514d7fefb1bc40e2cb4d199da609a
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-12 16:25:31 -0300

    Ident merge file

--- g-s-branch.sh
+++ g-s-branch.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-BRANCH=$(git branch | fzf +m --height 100% --layout reverse --border --preview 'git -c color.ui=always diff $(echo {} | tr -d " *") | less -R' --color bg:#222222,preview-bg:#333333)
+CURRENT_BRANCH=$(git branch | grep "^*" | tr -d "* ")
+
+BRANCH=$(git branch | fzf +m \
+    --height 100% \
+    --layout reverse \
+    --border \
+    --preview \
+        'git -c color.ui=always diff $(echo {} | tr -d " *")' \
+    --color bg:#222222,preview-bg:#333333)
+BRANCH=$(echo $BRANCH | tr -d " ")
 
 if [ $? -eq 0 ]; then
     git switch "$BRANCH"
